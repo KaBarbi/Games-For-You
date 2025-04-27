450d85b5f78ee5d51c7b8fef6af95c24a2304fe4
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-04-27 00:10:27 -0300

    [FEAT]: footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-links">
+                <a href="#">Home</a>
+                <a href="#">PlayStation</a>
+                <a href="#">Xbox</a>
+                <a href="#">Switch</a>
+            </div>
+            <div className="footer-copy">
+                <p>© 2025 Games4U, Inc. All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
