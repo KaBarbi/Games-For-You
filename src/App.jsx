7d4e60aba2aa8e3d5395d59e2e1ebb2747c76e6e
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-04-27 00:08:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer"
+import Menu from "./components/Menu"
+
+function App() {
+    return (
+        <>
+            <Menu />
+            <Footer />
+        </>
+    )
+}
+
+export default App
