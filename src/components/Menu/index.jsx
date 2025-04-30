f144aafef94178b3ed52fdc0553ef38843ebd823
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-04-29 22:08:55 -0300

    [FEAT]: search bar

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,28 +1,34 @@
 import React from "react"
 import { NavLink, Link } from "react-router-dom"
-import { FaShoppingCart } from "react-icons/fa"
+import { FaShoppingCart, FaSearch } from "react-icons/fa"
 import { MdAccountCircle } from "react-icons/md"
 import "./Menu.css"
 
 const Menu = () => {
     return (
         <header className="menu">
-            <div className="logo">
-                <Link to="/">
-                    <img src="/images/logo4.png" alt="Logo" />
-                </Link>
+            <div className="left-section">
+                <div className="logo">
+                    <Link to="/">
+                        <img src="/images/logo4.png" alt="Logo" />
+                    </Link>
+                </div>
+
+                <nav className="nav-links">
+                    <NavLink to="/" end>
+                        Home
+                    </NavLink>
+                    <NavLink to="/playstation">PlayStation</NavLink>
+                    <NavLink to="/xbox">Xbox</NavLink>
+                    <NavLink to="/switch">Switch</NavLink>
+                </nav>
             </div>
 
-            <nav className="nav-links">
-                <NavLink to="/" end>
-                    Home
-                </NavLink>
-                <NavLink to="/playstation">PlayStation</NavLink>
-                <NavLink to="/xbox">Xbox</NavLink>
-                <NavLink to="/switch">Switch</NavLink>
-            </nav>
-
-            <div className="icons">
+            <div className="right-section">
+                <div className="search-container">
+                    <FaSearch className="search-icon" />
+                    <input type="text" placeholder="Search..." />
+                </div>
                 <Link to="/login">
                     <MdAccountCircle size={28} />
                 </Link>
